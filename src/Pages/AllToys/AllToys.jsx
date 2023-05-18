d6babbabb90ce43810d6118a3aef3a24b8441b1d
Sakib <bksumon51@gmail.com>
2023-05-19 00:25:58 +0600

    added search box

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ToyDataRow from './ToyDataRow';
+import { FaSearch } from 'react-icons/fa';
 
 const AllToys = () => {
     const allToys= useLoaderData()
+    console.log(allToys);
+    const [toys, setToys] = useState(allToys)
+    const [searchText, setSearchText] = useState('')
+    const handelSearch = () => {
+     fetch(`http://localhost:5000/searchToy/${searchText}`).then(res => res.json()).then(data => setToys(data) )
+    }
+    console.log(searchText);
     return (
         <div>
             <h2 className=" tracking-wide font-bold text-center bg-gradient-to-r from-yellow-500 to-pink-500 text-transparent bg-clip-text text-5xl py-5"> All toys</h2>
             <div>
             <div className="overflow-x-auto">
+
+              {/* Search Box */}
+              <div className="bg-red-200 py-5 flex justify-center items-center w-full">
+  <div>
+  <div className="input-group bg-red-200">
+    <input onChange={(e) => setSearchText(e.target.value)} type="search" placeholder="Search…" className="input input-bordered" />
+    <button onClick={handelSearch} className="btn btn-square">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  </div>
+  </div>
+</div>
   <table className="table table-zebra w-full">
     {/* head */}
     <thead>
@@ -25,7 +45,7 @@ const AllToys = () => {
     <tbody>
       {/* row 1 */}
      {
-        allToys.map((toys, index) => <ToyDataRow key={toys._id} toys={toys} rowNum={index}/>)
+        toys?.map((toys, index) => <ToyDataRow key={toys._id} toys={toys} rowNum={index}/>)
      }
      
     </tbody>
